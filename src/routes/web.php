5bec9f18461a3d25aea61d51ac83b62e0f620a8d
Volha Pivavarchyk <volha@aivie.ch>
2023-07-17 20:25:56 +0300

    Remove auth controllers

--- src/routes/web.php
+++ src/routes/web.php
@@ -15,7 +15,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [\App\Http\Controllers\IndexController::class, 'index']);
 
-
-Auth::routes();
-
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
